f8c24b328a73f43752fd1c163b7f0621556498bb
tangpoo <lhinav@naver.com>
2024-03-18 16:17:25 +0900

    first commit

---
+++ src/test/java/me/tangpoo/troller/TrollerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.tangpoo.troller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrollerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
