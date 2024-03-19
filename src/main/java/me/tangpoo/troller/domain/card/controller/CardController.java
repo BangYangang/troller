17c0f67fa038bce91c7cbe8993b69e44871d1152
yallo428 <yallo428@naver.com>
2024-03-19 12:43:37 +0900

    :sparkles: add Card create, get, delete

--- src/main/java/me/tangpoo/troller/domain/card/controller/CardController.java
+++ src/main/java/me/tangpoo/troller/domain/card/controller/CardController.java
@@ -2,10 +2,54 @@ package me.tangpoo.troller.domain.card.controller;
 
 
 import lombok.RequiredArgsConstructor;
+import me.tangpoo.troller.domain.card.dto.CreateCardForm;
+import me.tangpoo.troller.domain.card.dto.ResponseCardForm;
+import me.tangpoo.troller.domain.card.service.CardService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/boards/{boardId}/todos/{todoId}/cards")
 public class CardController {
 
+  private final CardService cardService;
+
+  @PostMapping
+  public ResponseEntity<String> createCard(
+      @PathVariable Long boardId,
+      @PathVariable Long todoId,
+      @RequestParam CreateCardForm dto
+  ) {
+    return ResponseEntity.status(201)
+        .body(cardService.create(boardId, todoId, dto));
+  }
+
+  @GetMapping("/{cardId}")
+  public ResponseEntity<ResponseCardForm> getCard(
+      @PathVariable Long boardId,
+      @PathVariable Long todoId,
+      @PathVariable Long cardId
+  ) {
+
+    return ResponseEntity.status(201)
+        .body(cardService.get(boardId, todoId, cardId));
+  }
+
+  @DeleteMapping("/{cardId}")
+  public ResponseEntity<String> delete(
+      @PathVariable Long boardId,
+      @PathVariable Long todoId,
+      @PathVariable Long cardId
+  ){
+    return ResponseEntity.status(201)
+        .body(cardService.delete(boardId, todoId, cardId));
+  }
 }
--- src/main/java/me/tangpoo/troller/domain/card/service/CardService.java
+++ src/main/java/me/tangpoo/troller/domain/card/service/CardService.java
@@ -1,10 +1,102 @@
 package me.tangpoo.troller.domain.card.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import me.tangpoo.troller.domain.board.entity.Board;
+import me.tangpoo.troller.domain.board.repository.BoardRepository;
+import me.tangpoo.troller.domain.card.dto.CreateCardForm;
+import me.tangpoo.troller.domain.card.dto.ResponseCardForm;
+import me.tangpoo.troller.domain.card.entity.Card;
+import me.tangpoo.troller.domain.card.repository.CardRepository;
+import me.tangpoo.troller.domain.todo.entity.Todo;
+import me.tangpoo.troller.domain.todo.repository.TodoRepository;
+import me.tangpoo.troller.global.exception.custom.EntityNotMatchException;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class CardService {
+
+  private final CardRepository cardRepository;
+  private final BoardRepository boardRepository;
+  private final TodoRepository todoRepository;
+
+  public String create(Long boardId, Long todoId, CreateCardForm dto) {
+    Board board = findBoardBy(boardId);
+    Todo todo = findTodoBy(todoId);
+
+    boardMatchValidate(todo, board);
+
+    int orderNum = cardRepository.countCardsByTodo(todo) + 1;
+
+    cardRepository.save(
+        Card.builder()
+            .todo(todo)
+            .cardOrder(orderNum)
+            .color("black")
+            .name(dto.getName())
+            .description(dto.getDescription())
+            .build()
+        );
+
+    return "카드 작성을 완료했습니다.";
+  }
+
+  public ResponseCardForm get(Long boardId, Long todoId, Long cardId) {
+    Board board = findBoardBy(boardId);
+    Todo todo = findTodoBy(todoId);
+    Card card = findCardBy(cardId);
+
+    boardMatchValidate(todo, board);
+    todoMatchValidate(card, todo);
+
+    return card.createCardResponseDto();
+  }
+
+  public String delete(Long boardId, Long todoId, Long cardId) {
+    Board board = findBoardBy(boardId);
+    Todo todo = findTodoBy(todoId);
+    Card card = findCardBy(cardId);
+
+    boardMatchValidate(todo, board);
+    todoMatchValidate(card, todo);
+
+
+    cardRepository.delete(card);
+    return "삭제가 완료되었습니다!";
+  }
+
+
+
+  private Card findCardBy(Long cardId) {
+    return cardRepository.findById(cardId).orElseThrow(
+        () -> new EntityNotFoundException("해당 카드를 찾을 수 없습니다.")
+    );
+  }
+
+  private Todo findTodoBy(Long todoId) {
+    return todoRepository.findById(todoId).orElseThrow(
+        () -> new EntityNotFoundException("해당 컬럼을 찾을 수 없습니다.")
+    );
+  }
+
+  private Board findBoardBy(Long boardId) {
+    return boardRepository.findById(boardId).orElseThrow(
+        () -> new EntityNotFoundException("해당 보드를 찾을 수 없습니다.")
+    );
+  }
+
+  private void boardMatchValidate(Todo todo, Board board) {
+    if(todo.isNotBoardMatch(board)){
+      throw new EntityNotMatchException("해당 보드에 속한 컬럼이 아닙니다.");
+    }
+  }
+
+  private void todoMatchValidate(Card card, Todo todo) {
+    if(card.isNotTodoMatch(todo)){
+      throw new EntityNotMatchException("해당 컬럼에 속한 카드가 아닙니다.");
+    }
+  }
+
 
 }
