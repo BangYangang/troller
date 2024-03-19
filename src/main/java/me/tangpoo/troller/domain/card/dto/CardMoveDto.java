fd4444056978fdc9f91a5fea5edf02a1edfb3226
yallo428 <yallo428@naver.com>
2024-03-19 15:59:41 +0900

    :sparkles: add moveDto

---
+++ src/main/java/me/tangpoo/troller/domain/card/dto/CardMoveDto.java
@@ -0,0 +1,12 @@
+package me.tangpoo.troller.domain.card.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CardMoveDto {
+
+}
