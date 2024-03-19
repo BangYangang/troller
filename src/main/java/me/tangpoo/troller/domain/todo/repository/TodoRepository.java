69713bd7ba9b77ee58b183d88dcb6dcdb15b45c0
yallo428 <yallo428@naver.com>
2024-03-19 14:13:08 +0900

    :truck: rename Todorepository -> TodoRepository

--- src/main/java/me/tangpoo/troller/domain/todo/repository/TodoRepository.java
+++ src/main/java/me/tangpoo/troller/domain/todo/repository/TodoRepository.java
@@ -3,6 +3,8 @@ package me.tangpoo.troller.domain.todo.repository;
 import me.tangpoo.troller.domain.todo.entity.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+import java.util.List;
 
+public interface TodoRepository extends JpaRepository<Todo,Long> {
+    List<Todo> findByBoard_BoardId(Long boardId);
 }
