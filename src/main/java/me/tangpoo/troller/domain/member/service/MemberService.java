0114c7b8918991802f7858c861e706abedf7a54a
tangpoo <lhinav@naver.com>
2024-03-19 15:34:18 +0900

    :recycle: update password encoder 문제, 정보 조회 필드 null 문제 해결

--- src/main/java/me/tangpoo/troller/domain/member/service/MemberService.java
+++ src/main/java/me/tangpoo/troller/domain/member/service/MemberService.java
@@ -1,11 +1,18 @@
 package me.tangpoo.troller.domain.member.service;
 
+import static com.querydsl.core.types.Projections.fields;
+import static me.tangpoo.troller.domain.member.entity.QMember.member;
+
+import com.querydsl.core.QueryFactory;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
 import me.tangpoo.troller.domain.member.dto.MemberResponse;
 import me.tangpoo.troller.domain.member.dto.MemberRequest;
 import me.tangpoo.troller.domain.member.entity.Member;
+import me.tangpoo.troller.domain.member.entity.RefreshToken;
 import me.tangpoo.troller.domain.member.repository.MemberRepository;
+import me.tangpoo.troller.domain.member.repository.RefreshTokenRepository;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -18,6 +25,8 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JPAQueryFactory queryFactory;
+    private final RefreshTokenRepository refreshTokenRepository;
 
     @Transactional
     public void saveMember(MemberRequest memberRequest){
@@ -39,9 +48,12 @@ public class MemberService {
     }
 
     public MemberResponse getMember(Long memberId) {
-        Member findMember = getFindMember(memberId);
-
-        return new MemberResponse(findMember.getUsername(), findMember.getUsername());
+        return queryFactory.select(fields(MemberResponse.class,
+            member.username,
+            member.email))
+            .from(member)
+            .where(member.memberId.eq(memberId))
+            .fetchOne();
     }
 
 
@@ -50,9 +62,10 @@ public class MemberService {
     public void updateMember(Long memberId, MemberRequest memberRequest) {
         Member findMember = getFindMember(memberId);
 
-        findMember.update(memberRequest.getUsername(),
+        findMember.update(
+            memberRequest.getUsername(),
             memberRequest.getEmail(),
-            memberRequest.getPassword()
+            passwordEncoder.encode(memberRequest.getPassword())
         );
     }
 
