44c6806e2537276eb814306fdfeab8a7523ee506
pokly0908 <pokly0908@naver.com>
2024-03-19 13:03:21 +0900

    :sparkles: Code Convention

--- src/main/java/me/tangpoo/troller/domain/todo/dto/TodoRequestDto.java
+++ src/main/java/me/tangpoo/troller/domain/todo/dto/TodoRequestDto.java
@@ -8,6 +8,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class TodoRequestDto {
+
     private String todoName;
 
     public TodoRequestDto(String todoName) {
--- src/main/java/me/tangpoo/troller/domain/todo/repository/Todorepository.java
+++ src/main/java/me/tangpoo/troller/domain/todo/repository/Todorepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface Todorepository extends JpaRepository<Todo,Long> {
+public interface Todorepository extends JpaRepository<Todo, Long> {
+
     List<Todo> findByBoard_BoardId(Long boardId);
 }
