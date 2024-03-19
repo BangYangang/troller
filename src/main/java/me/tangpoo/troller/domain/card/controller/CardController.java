cb66f486c1765a5928a9c3b160af8e5d35f58a17
yallo428 <yallo428@naver.com>
2024-03-19 20:46:59 +0900

    :recycle: add @AuthenticationPrincipal

--- src/main/java/me/tangpoo/troller/domain/card/controller/CardController.java
+++ src/main/java/me/tangpoo/troller/domain/card/controller/CardController.java
@@ -3,10 +3,13 @@ package me.tangpoo.troller.domain.card.controller;
 
 import lombok.RequiredArgsConstructor;
 import me.tangpoo.troller.domain.card.dto.CreateCardForm;
+import me.tangpoo.troller.domain.card.dto.ResponseCardDetailForm;
 import me.tangpoo.troller.domain.card.dto.ResponseCardForm;
 import me.tangpoo.troller.domain.card.dto.UpdateCardForm;
 import me.tangpoo.troller.domain.card.service.CardService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,43 +30,47 @@ public class CardController {
 
   @PostMapping
   public ResponseEntity<String> createCard(
+      @AuthenticationPrincipal UserDetails userDetails,
       @PathVariable Long boardId,
       @PathVariable Long todoId,
       @Validated @RequestBody CreateCardForm dto
   ) {
     return ResponseEntity.status(201)
-        .body(cardService.create(boardId, todoId, dto));
+        .body(cardService.create(boardId, todoId, dto, userDetails.getUsername()));
   }
 
   @GetMapping("/{cardId}")
-  public ResponseEntity<ResponseCardForm> getCard(
+  public ResponseEntity<ResponseCardDetailForm> getCard(
+      @AuthenticationPrincipal UserDetails userDetails,
       @PathVariable Long boardId,
       @PathVariable Long todoId,
       @PathVariable Long cardId
   ) {
 
     return ResponseEntity.status(201)
-        .body(cardService.get(boardId, todoId, cardId));
+        .body(cardService.get(boardId, todoId, cardId, userDetails.getUsername()));
   }
 
   @PutMapping("/{cardId}")
   public ResponseEntity<ResponseCardForm> updateCard(
+      @AuthenticationPrincipal UserDetails userDetails,
       @PathVariable Long boardId,
       @PathVariable Long cardId,
       @PathVariable Long todoId,
       @Validated @RequestBody UpdateCardForm dto
   ){
     return ResponseEntity.status(201)
-        .body(cardService.update(boardId, cardId, todoId, dto));
+        .body(cardService.update(boardId, cardId, todoId, dto, userDetails.getUsername()));
   }
 
   @DeleteMapping("/{cardId}")
   public ResponseEntity<String> delete(
+      @AuthenticationPrincipal UserDetails userDetails,
       @PathVariable Long boardId,
       @PathVariable Long todoId,
       @PathVariable Long cardId
   ){
     return ResponseEntity.status(201)
-        .body(cardService.delete(boardId, todoId, cardId));
+        .body(cardService.delete(boardId, todoId, cardId, userDetails.getUsername()));
   }
 }
